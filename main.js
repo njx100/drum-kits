b42d287bc19228e4426323a39c4ed495d08a9840
njx100 <dnnguyen100@gmail.com>
2023-04-04 16:20:29 +0700

    complete all functions

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+const kits = ["crash", "kick", "snare", "tom"];
+const containerEl = document.querySelector(".container");
+
+
+kits.forEach( kit => {
+    const btnEl = document.createElement("button");
+    btnEl.classList.add("btn");
+    btnEl.innerText = kit;
+    btnEl.style.background = `url("images/${kit}.png")`;
+    containerEl.appendChild(btnEl);
+    const audioEl = document.createElement("audio");
+    audioEl.src = `sounds/${kit}.mp3`;
+    containerEl.appendChild(audioEl);
+    btnEl.addEventListener("click", () => {
+        audioEl.play();
+    });
+    window.addEventListener("keydown", (event) => {
+    if (event.key === kit.charAt(0)) {
+        audioEl.play();
+        btnEl.style.transform = "scale(.9)";
+        setTimeout(() => {
+            btnEl.style.transform = "scale(1)";
+        }, 100);
+        };
+    });
+    
+});
+
+
+
 
