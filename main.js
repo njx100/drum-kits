07e7d104d2d401d144670b17d57739f5ad32ae48
njx100 <dnnguyen100@gmail.com>
2023-04-04 18:27:07 +0700

    revision 1, added animation to click-eventlistener

--- main.js
+++ main.js
@@ -2,17 +2,23 @@ const kits = ["crash", "kick", "snare", "tom"];
 const containerEl = document.querySelector(".container");
 
 
+
 kits.forEach( kit => {
     const btnEl = document.createElement("button");
     btnEl.classList.add("btn");
     btnEl.innerText = kit;
     btnEl.style.background = `url("images/${kit}.png")`;
+    btnEl.style.backgroundSize = "cover";
     containerEl.appendChild(btnEl);
     const audioEl = document.createElement("audio");
     audioEl.src = `sounds/${kit}.mp3`;
     containerEl.appendChild(audioEl);
     btnEl.addEventListener("click", () => {
         audioEl.play();
+        btnEl.style.transform = "scale(.9)";
+        setTimeout(() => {
+            btnEl.style.transform = "scale(1)";
+        }, 50);
     });
     window.addEventListener("keydown", (event) => {
     if (event.key === kit.charAt(0)) {
@@ -20,12 +26,8 @@ kits.forEach( kit => {
         btnEl.style.transform = "scale(.9)";
         setTimeout(() => {
             btnEl.style.transform = "scale(1)";
-        }, 100);
+        }, 50);
         };
     });
     
 });
-
-
-
-
